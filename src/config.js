2eb233bff1a9350c0de504a7c8999414e13c4486
nikitabelous <nikitabelous16@gmail.com>
2023-09-06 19:40:17 +0300

    Revert "wip: hero section"
    This reverts commit 947a50a0fff1f13a6721a41023e585298da7565e.

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const CONFIG = {
-    initLoaderDelay: 1000,
-
-};
--- src/const/index.js
+++
@@ -1,4 +0,0 @@
-export * from './component-node-ids';
-export * from './routes';
-export * from './theme';
-export * from './transition.js'
