b8a6fe1da6980c0dbb19948ec6f5a363ee4033e9
nikitabelous <nikitabelous16@gmail.com>
2023-09-08 15:22:08 +0300

    wip animate title

--- src/components/HomeHeroSection/HomeHeroSection.jsx
+++ src/components/HomeHeroSection/HomeHeroSection.jsx
@@ -1,5 +1,6 @@
 import './HomeHeroSection.scss';
 
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { HomeHeroInstallSection, Icon } from '@/components';
@@ -7,7 +8,23 @@ import { COMPONENT_NODE_IDS } from '@/const';
 
 const HomeHeroSection = () => {
   const { t } = useTranslation();
-
+  const refAnimateTitleBefore = useRef(null);
+  const refAnimateTitleAfter = useRef(null);
+  useEffect(() => {
+    setTimeout(() => {
+      refAnimateTitleBefore.current.style.color = 'transparent';
+      refAnimateTitleAfter.current.style.position = 'absolute';
+      refAnimateTitleAfter.current.style.left = '20px';
+      setTimeout(() => {
+        refAnimateTitleAfter.current.style.position = 'relative';
+        refAnimateTitleAfter.current.style.left = '0';
+        refAnimateTitleBefore.current.style.display = 'none';
+      }, 375);
+      refAnimateTitleAfter.current.style.display = '';
+      refAnimateTitleAfter.current.className =
+        'home-hero-section__title-part-2-animation home-hero-section__title-part-2';
+    }, 1000);
+  }, []);
   return (
     <section id={COMPONENT_NODE_IDS.heroSection} className="home-hero-section">
       <div className="home-hero-section__content container">
@@ -16,8 +33,20 @@ const HomeHeroSection = () => {
             <h2 className="home-hero-section__title">
               <div className="home-hero-section__title-part">
                 <span>{t('home-hero-section.title-part-1')}</span>
-                <div className="home-hero-section__title-part-2">
-                  <span>{t('home-hero-section.title-part-2')}</span>
+                <div className="home-hero-section__title-part-2-wrapper">
+                  <span
+                    ref={refAnimateTitleBefore}
+                    className="home-hero-section__title-part-2"
+                  >
+                    {t('home-hero-section.title-part-2')}
+                  </span>
+                  <span
+                    ref={refAnimateTitleAfter}
+                    className="home-hero-section__title-part-2"
+                    style={{ display: 'none' }}
+                  >
+                    {t('home-hero-section.title-part-2-animation')}
+                  </span>
                   <span className="home-hero-section__title-part-2-after">
                     {t('home-hero-section.title-part-2-after')}
                   </span>
