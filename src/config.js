919ca91f4436a34f00d24d7419785a4eedee4e0b
nikitabelous <nikitabelous16@gmail.com>
2023-09-08 17:51:53 +0300

    feat add link to halborn

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export const CONFIG = {
   initLoaderDelay: 1000,
   linkToInstallSnap: ' https://snaps.directory/snap/@rarimo/rarime',
+  linkToHalborn:
+    'https://github.com/HalbornSecurity/PublicReports/blob/master/Web%20Pentest/Rarimo_Snap_App_WebApp_Pentest_Report_Halborn_Final.pdf',
 };
