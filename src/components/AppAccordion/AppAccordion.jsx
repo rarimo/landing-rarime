ba6c73895831e00f81560727ea1a7ff416580e11
Nikita Belous <nikitabelous16@gmail.com>
2023-09-12 16:08:37 +0000

    Development

--- src/components/AppAccordion/AppAccordion.jsx
+++ src/components/AppAccordion/AppAccordion.jsx
@@ -4,32 +4,51 @@ import { useMemo, useState } from 'react';
 
 import Icon from '@/components/Icon';
 
-const AppAccordion = ({ headerText, contentText }) => {
-    const [ isExpanded, setIsExpanded ] = useState(false)
+const AppAccordion = ({ headerText, contentText, link, linkText }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const accordionClasses = useMemo(() => [
-      'app-accordion',
-      ...(isExpanded ? ['app-accordion--expanded'] : [])
-    ].join(' '), [isExpanded])
+  const accordionClasses = useMemo(
+    () =>
+      [
+        'app-accordion',
+        ...(isExpanded ? ['app-accordion--expanded'] : []),
+      ].join(' '),
+    [isExpanded],
+  );
 
-    const toogleIsExpanded = () => { setIsExpanded(!isExpanded) }
+  const toogleIsExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-    return (
-      <div className={accordionClasses}>
-        <div className="app-accordion__wrp">
-          <h2 className="app-accordion__header">
-            <button
-              className="app-accordion__header-btn"
-              onClick={toogleIsExpanded}
+  return (
+    <div className={accordionClasses}>
+      <div className="app-accordion__wrp">
+        <h2 className="app-accordion__header">
+          <button
+            className="app-accordion__header-btn"
+            onClick={toogleIsExpanded}
+          >
+            {headerText}
+            <Icon idIcon="icon-arrow-down" iconClass="app-accordion__icon" />
+          </button>
+        </h2>
+        {isExpanded && (
+          <p className="app-accordion__body">
+            {contentText}
+            <a
+              className="app-accordion__body-link"
+              href={link}
+              target="_blank"
+              rel="noreferrer"
             >
-              {headerText}
-              <Icon idIcon="icon-arrow-down" iconClass="app-accordion__icon" />
-            </button>
-          </h2>
-          {isExpanded && <p className="app-accordion__body">{contentText}</p>}
-        </div>
+              {' '}
+              {linkText}
+            </a>
+          </p>
+        )}
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default AppAccordion
+export default AppAccordion;
