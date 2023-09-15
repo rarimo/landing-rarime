eea22c14f283d0a9b575b2162038e7e7d9123221
nikitabelous <nikitabelous16@gmail.com>
2023-09-15 11:15:45 +0300

    update logo and link to snap

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   initLoaderDelay: 1000,
-  linkToInstallSnap: ' https://snaps.directory/snap/@rarimo/rarime',
+  linkToInstallSnap: 'https://snaps.metamask.io/snap/npm/rarimo/rarime/',
   linkToHalborn:
     'https://github.com/HalbornSecurity/PublicReports/blob/master/Web%20Pentest/Rarimo_Snap_App_WebApp_Pentest_Report_Halborn_Final.pdf',
   discordLink: 'https://discord.gg/Bzjm5MDXrU',
