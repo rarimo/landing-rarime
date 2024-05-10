94b1ef6ac65df8b77b02c32b5d9d7abb37fd93d2
ardier16 <ardier16@gmail.com>
2024-05-10 15:14:18 +0300

    Add about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,3 +1,26 @@
 export default function AboutSection() {
-  return <div className='container'>About section</div>
+  return (
+    <div className='container flex flex-col items-center'>
+      <img src='/images/about-phone.png' alt='About Section' className='w-74' />
+      <div className='-mt-40 flex max-w-[840px] flex-col items-center gap-20'>
+        <div className='flex flex-col items-center gap-6'>
+          <h2 className='typo-subtitle1 text-text-secondary'>About</h2>
+          <p className='typo-h3 text-center text-text-primary'>
+            Reclaim your online privacy as incognito, making your web activity
+            invisible and unlinkable to you
+          </p>
+        </div>
+        <div className='flex w-[400px] justify-between'>
+          <div className='flex flex-col items-center gap-6'>
+            <p className='typo-subtitle3 text-text-secondary'>Incognitos</p>
+            <p className='typo-h4 text-primary-dark'>5,924,252</p>
+          </div>
+          <div className='flex flex-col items-center gap-6'>
+            <p className='typo-subtitle3 text-text-secondary'>Identifiers</p>
+            <p className='typo-h4 text-primary-dark'>5,924,252</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
