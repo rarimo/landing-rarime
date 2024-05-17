d5e47a414750d30be785e1c7761e18f04847bad2
ardier16 <ardier16@gmail.com>
2024-05-17 13:31:46 +0300

    Update header

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -25,13 +25,13 @@ export default function HeroSection() {
           style={{ opacity: blockOpacity }}
         >
           <div className='mx-auto flex flex-col items-center gap-4 text-center'>
-            <span className='text-overline1 mx-auto w-max rounded-full bg-primary-base px-4 py-2 text-base-black'>
+            <span className='mx-auto w-max rounded-full bg-primary-base px-4 py-2 text-overline1 text-base-black'>
               RARIME APP
             </span>
-            <h1 className='text-h2 lg:text-h1 text-text-primary'>
+            <h1 className='text-h2 text-text-primary lg:text-h1'>
               GO INCOGNITO
             </h1>
-            <p className='text-body2 lg:text-body1 max-w-[720px] text-text-secondary'>
+            <p className='max-w-[720px] text-body2 text-text-secondary lg:text-body1'>
               RariMe is a self-custody identity wallet that allows users to
               verify their eligibility for Web3 services without exposing their
               personal data
@@ -40,7 +40,7 @@ export default function HeroSection() {
           </div>
         </div>
       </div>
-      <div className='3xl:relative 3xl:mt-40 mt-20 lg:absolute lg:bottom-10 lg:left-1/2 lg:mt-0 lg:-translate-x-1/2'>
+      <div className='mt-20 lg:absolute lg:bottom-10 lg:left-1/2 lg:mt-0 lg:-translate-x-1/2 3xl:relative 3xl:mt-40'>
         <div className='flex flex-col items-center gap-1' data-aos='fade-up'>
           <p className='text-center text-xs text-text-secondary'>Scroll</p>
           <ArrowsIcon className='h-5 w-5 animate-bounce text-text-secondary' />
--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -11,37 +11,39 @@ export default function Header() {
 
   return (
     <header
-      className='container sticky top-0 z-50 mt-1 flex flex-col gap-4 bg-white bg-opacity-80 pt-5 backdrop-blur-md'
+      className='sticky top-0 z-50 mt-1 bg-white bg-opacity-80 pt-5 backdrop-blur-md'
       data-aos='fade-up'
     >
-      <div className='flex justify-between text-text-primary'>
-        <div className='flex items-center gap-10'>
-          <a href='/#'>
-            <LogoIcon className='h-8' />
-          </a>
-          <div className='hidden items-center gap-8 lg:flex'>
-            {headerLinks.map(({ label, href }) => (
-              <a key={label} href={href} className='text-button-medium'>
-                {label}
-              </a>
-            ))}
+      <div className='container flex flex-col gap-4'>
+        <div className='flex justify-between text-text-primary'>
+          <div className='flex items-center gap-10'>
+            <a href='/#'>
+              <LogoIcon className='h-8' />
+            </a>
+            <div className='hidden items-center gap-8 lg:flex'>
+              {headerLinks.map(({ label, href }) => (
+                <a key={label} href={href} className='text-button-medium'>
+                  {label}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className='flex items-center gap-4'>
+            <a
+              className='button-medium button-outlined w-30 lg:w-max'
+              href='https://app.rarime.com'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Dashboard
+            </a>
+            <button className='flex h-10 w-10 items-center justify-center rounded-full bg-background-component lg:hidden'>
+              <MenuIcon className='h-5' />
+            </button>
           </div>
         </div>
-        <div className='flex items-center gap-4'>
-          <a
-            className='button-medium button-outlined w-30 lg:w-max'
-            href='https://app.rarime.com'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Dashboard
-          </a>
-          <button className='flex h-10 w-10 items-center justify-center rounded-full bg-background-component lg:hidden'>
-            <MenuIcon className='h-5' />
-          </button>
-        </div>
+        <div className='divider-h' />
       </div>
-      <div className='divider-h' />
     </header>
   )
 }
