b604931f6076068823502276946b7e75ec9c6072
ardier16 <ardier16@gmail.com>
2025-03-21 14:51:58 +0200

    Redirect home page to rarimo.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,68 +1,9 @@
-import '@/theme/styles.scss'
-
-import type { Metadata } from 'next'
 import { ReactNode } from 'react'
-
-import AosInit from '@/common/AosInit'
-import { appFontClassName } from '@/theme/fonts'
-import { cn } from '@/theme/utils'
-
-export const metadata: Metadata = {
-  metadataBase: new URL('https://rarime.com/'),
-  title: 'Go Incognito | RariMe',
-  description:
-    'RariMe is a self-custody identity wallet, that unlocks private but verifiable identities',
-
-  openGraph: {
-    title: 'Go Incognito | RariMe',
-    description:
-      'RariMe is a self-custody identity wallet, that unlocks private but verifiable identities',
-    type: 'website',
-    url: 'https://rarime.com/',
-    siteName: 'RariMe',
-    images: '/images/og-image.png',
-  },
-
-  twitter: {
-    title: 'Go Incognito | RariMe',
-    description:
-      'RariMe is a self-custody identity wallet, that unlocks private but verifiable identities',
-    card: 'summary_large_image',
-    images: '/images/og-image.png',
-    site: 'rarime.com',
-  },
-}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html
-      lang='en'
-      className={cn(
-        appFontClassName,
-        'scroll-pt-20 scroll-smooth bg-background-container font-primary',
-      )}
-    >
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `history.scrollRestoration = "manual"`,
-          }}
-        />
-        <link
-          href='/favicon/favicon-dark.ico'
-          rel='icon'
-          media='(prefers-color-scheme: light)'
-        />
-        <link
-          href='/favicon/favicon-light.ico'
-          rel='icon'
-          media='(prefers-color-scheme: dark)'
-        />
-      </head>
-      <body>
-        <AosInit />
-        {children}
-      </body>
+    <html lang='en'>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,5 @@
-import Footer from '@/common/Footer'
-import Header from '@/common/Header'
-import { WindowScrollProvider } from '@/contexts/window-scroll'
-
-import AboutSection from './components/AboutSection'
-import CardsSection from './components/CardsSection'
-import HeroSection from './components/HeroSection'
-import IntegrationsSection from './components/IntegrationsSection'
-import PassportsCard from './components/PassportsCard'
-import RewardsSection from './components/RewardsSection'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className='mb-25 flex flex-col gap-25 lg:mb-50 lg:gap-50'>
-        <WindowScrollProvider>
-          <HeroSection />
-          <AboutSection />
-          <PassportsCard />
-          <RewardsSection />
-          <CardsSection />
-          <IntegrationsSection />
-        </WindowScrollProvider>
-      </main>
-      <Footer />
-    </>
-  )
+  redirect('https://rarimo.com')
 }
